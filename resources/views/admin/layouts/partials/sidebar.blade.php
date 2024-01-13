560c7a62030e6e71008b1ae91db8522f76b30bf7
yasin taghavi <mrtqvi@yahoo.com>
2024-01-13 03:54:52 +0330

    work on the dynamic admin dashboard

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -21,6 +21,13 @@
         <div class="nk-sidebar-content">
             <div class="nk-sidebar-menu" data-simplebar>
                 <ul class="nk-menu">
+                    <li class="nk-menu-item">
+                        <a href="{{ route('admin.index') }}" class="nk-menu-link">
+                            <span class="nk-menu-icon"><em class="icon ni ni-dashboard-fill"></em></span>
+
+                            <span class="nk-menu-text">داشبورد</span>
+                        </a>
+                    </li>
                     <!-- .nk-menu-item -->
                     <li class="nk-menu-heading">
                         <h6 class="overline-title text-primary-alt">فروشگاه</h6>
